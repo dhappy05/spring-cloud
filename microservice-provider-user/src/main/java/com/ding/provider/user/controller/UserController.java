00d828e9a235a91efadfaeb7aaa4b6f796b78fe4
dhappy05 <dhappy05@126.com>
2018-04-19 14:45:40 +0800

    还原误提交的代码

--- microservice-provider-user/src/main/java/com/ding/provider/user/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/ding/provider/user/controller/UserController.java
@@ -1,4 +1,3 @@
-/*
 package com.ding.provider.user.controller;
 
 import com.ding.provider.user.entity.User;
@@ -10,22 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-*/
-/**
- * Created by ding on 2018/4/17.
- *//*
-
-
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/user/{id}")//本注解表示该该方法接爱一get请求
+    @GetMapping("/{id}")//本注解表示该该方法接爱一get请求
     public User findById(@PathVariable Long id){
         Optional<User> user = this.userRepository.findById(id);
         return user.get();
     }
 
 }
-*/
