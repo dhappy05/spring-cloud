9aae6c7adbb75cf01a212232222a45cc74ed64e3
dhappy05 <dhappy05@126.com>
2018-04-23 08:44:08 +0800

    使用Turbine聚合监控数据

---
+++ microservice-hystrix-turbine/src/main/java/com/ding/hystrix/turbine/MicroserviceHystrixTurbineApplication.java
@@ -0,0 +1,14 @@
+package com.ding.hystrix.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class MicroserviceHystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceHystrixTurbineApplication.class, args);
+	}
+}
---
+++ microservice-hystrix-turbine/src/test/java/com/ding/hystrix/turbine/MicroserviceHystrixTurbineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ding.hystrix.turbine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceHystrixTurbineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
